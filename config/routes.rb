37b3d0d936a89a1135d6151000799ff556ce7921
KaylaFincher <kayladfincher@gmail.com>
2024-09-03 21:57:39 -0500

    Generated basic CRUD app

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :books do
+    member do 
+      get :delete
+    end
+  end
+end
